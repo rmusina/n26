14c99a1f09cfcf92d484167c2f54959f344bd4cd
rmusina <rmusina@microsoft.com>
2016-11-17 17:18:15 +0100

    Added unit tests for empty bucket cases.

--- src/test/java/com/n26/backend/statistics/BucketMergerTests.java
+++ src/test/java/com/n26/backend/statistics/BucketMergerTests.java
@@ -2,22 +2,45 @@ package com.n26.backend.statistics;
 
 import com.n26.backend.time.EpochTime;
 import com.n26.backend.time.FixedTimeInterval;
+import com.n26.backend.time.Time;
+import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.when;
 
 public class BucketMergerTests {
     @Test
     public void whenAddingAValidBucketOverAnEmptyBucketThenTheNewBucketReplacesTheEmptyOne() {
-        BucketMerger merger = new BucketMerger(new FixedTimeInterval(60, new EpochTime(60000)));
+        FixedTimeInterval timeInterval = Mockito.mock(FixedTimeInterval.class);
+        when(timeInterval.isInTimeInterval(isA(Time.class))).thenReturn(true);
+        BucketMerger merger = new BucketMerger(timeInterval);
+
+        Bucket newBucket = new TimeBasedBucket(new EpochTime(200), new EmptyStatistics());
+        Bucket result = merger.apply(new EmptyBucket(), newBucket);
+
+        Assert.assertSame(result, newBucket);
     }
 
     @Test
     public void whenAddingAnInvalidBucketOverAnEmptyBucketThenTheBucketRemainsEmpty() {
+        FixedTimeInterval timeInterval = Mockito.mock(FixedTimeInterval.class);
+        when(timeInterval.isInTimeInterval(isA(Time.class))).thenReturn(false);
+        BucketMerger merger = new BucketMerger(timeInterval);
+
+        Bucket newBucket = new TimeBasedBucket(new EpochTime(200), new EmptyStatistics());
+        Bucket emptyBucket = new EmptyBucket();
+        Bucket result = merger.apply(emptyBucket, newBucket);
+
+        Assert.assertEquals(EmptyBucket.class, result.getClass() );
     }
 
     @Test
     public void whenAddingAnInvalidBucketOverAValidBucketThenTheValidBucketIsNotChanged() {
     }
 
+    @Test
     public void whenAddingAValidBucketOverAnotherValidBucketThenTheBucketsAreAggregated() {
 
     }
